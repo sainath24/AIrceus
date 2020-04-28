95e0ecfeef2ba00d98f78c072692cc215a3301fa
Sainath Prasanna <sainathprasanna@gmail.com>
2020-04-29 01:28:29 +0530

    switched to explicit waits, added some checks

--- match.py
+++ match.py
@@ -2,24 +2,25 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import pokemon_data as pd
 
 my_pokemon = []
+enemy_pokemon = []
 opp_pokemon = None
-
-
 
 browser = webdriver.Edge('C:/Users/saina/Files/projects/PokemonShowdownAI/edge_driver/msedgedriver.exe')
 browser.get('https://play.pokemonshowdown.com/')
 
 actions = ActionChains(browser)
 
-browser.implicitly_wait(60)
-login_button = browser.find_element_by_name('login')
+login_button = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, "login")))#browser.find_element_by_name('login')
 login_button.click() #click login button
 
-username = browser.find_element_by_name('username')
+username = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, "username")))#browser.find_element_by_name('username')
 username.send_keys('saroja')
 username.send_keys(Keys.ENTER) #login with a name
 
@@ -48,20 +49,111 @@ time.sleep(2)
 # username.send_keys('saroja2')
 # username.send_keys(Keys.ENTER)
 time.sleep(10)
-accept_challenge_button = browser.find_element_by_name('acceptChallenge')
+accept_challenge_button = WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.NAME, "acceptChallenge")))#browser.find_element_by_name('acceptChallenge')
 time.sleep(5)
 accept_challenge_button.click()
 
 
-active_pokemon = pd.getActivePokemon(browser,actions)
+active_pokemon = pd.getActivePokemon(browser)
 my_pokemon.append(active_pokemon)
 
-inactive_pokemon = pd.getInactivePokemon(browser,actions)
+inactive_pokemon = pd.getInactivePokemon(browser)
 my_pokemon.extend(inactive_pokemon)
 
 print('My_Pokemon: ' + str(my_pokemon))
 
-opp_pokemon = pd.getActiveEnemyPokemon(browser,actions)
+opp_pokemon = pd.getActiveEnemyPokemon(browser)
 print(opp_pokemon)
+
+game = {
+    'active' : True,
+    'forfeit' : False,
+    'active_pokemon': 0,
+    'active_pokemon_name': active_pokemon['name'],
+    'enemy_active_pokemon_name': opp_pokemon['name'],
+    'enemy_pokemon': 6,
+    'my_pokemon': 6
+}
+
+while game['active']:
+
+    if game['my_pokemon'] == 0 or game['enemy_pokemon'] == 0:
+        game['active'] = False
+        break
+
+    opp_pokemon_new = pd.getActiveEnemyPokemon(browser)
+    print('Got enemy opp_pokemon_new')
+
+    if opp_pokemon_new == None or opp_pokemon_new['name'] != opp_pokemon['name']:
+        enemy_pokemon,enemyFaintCount = pd.getEnemyPokemonList(browser,actions)
+        game['enemy_pokemon'] = 6 - enemyFaintCount
+        if game['enemy_pokemon'] == 0:
+            game['active'] = False
+            break
+        opp_pokemon = opp_pokemon_new
+        game['enemy_active_pokemon_name'] = opp_pokemon['name']
+
+    
+    # TODO:make moves, get updates
+    # network makes a 
+    print('\nmake move\n')
+    time.sleep(40)
+    print('\nMove over\n')
+    
+    # explicitly wait for 
+    move_switch = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.NAME, "selectMove")) or EC.presence_of_element_located((By.NAME,"selectSwitch")))#browser.find_element_by_name('selectMove') or browser.find_element_by_name('selectSwitch')
+    if move_switch.text == 'Attack': #choose next attack
+        print('\nIn attack\n')
+        #TODO: get updates, calculate reward
+        active_pokemon_new = pd.getActivePokemon(browser)
+        # calculate reward
+        my_pokemon[game['active_pokemon']] = active_pokemon_new
+        print('Active Pokemon:' + str(active_pokemon))
+        # TODO: network decides to either switch or attack
+        # click based on network, if switch use pokemon['element']
+    
+    elif move_switch.text == 'Switch': #choose pokemon to switch to
+        # check if active pokemon is dead
+        print('\nIn Switch\n')
+        active_pokemon_new = pd.getActivePokemon(browser)
+        if active_pokemon_new['isAlive'] == False: #pokemon has fainted
+            # TODO: calculate reward
+            # remove active pokemon from list
+            my_pokemon.remove(game['active_pokemon'])
+            game['my_pokemon']-=1
+            if game['my_pokemon'] == 0:
+                game['active'] = False
+                break
+        
+        # get updates
+        print('POKEMON: ' + active_pokemon_new)
+        # TODO: calculate reward from active_pokemon and active_pokemon_new 
+        # choose pokemon to switch
+        # TODO: neural network chooses switch, switch with pokemon['element']
+        print('\nchoose switch\n')
+        time.sleep(30)
+        print('\nSwitch chosen\n')
+
+if game['active'] == False:
+    if game['my_pokemon'] == 0 and game['enemy_pokemon'] > 0:
+        print('You lost')
+        # TODO: decrease rewards
+    
+    elif game['my_pokemon'] > 0 and game['enemy_pokemon'] == 0:
+        print('You win')
+        # TODO: increse reward
+    else:
+        print('Draw')
+        # TODO: manipulate reward, increase by a little
+
+else:
+    print('Broke because of some error') 
+    # TODO: handle later
+
+# div class controls text Waiting for opponent...
+# button name selectMove text Attack
+# button name selectSwitch text Switch (After faint or switch)
+#   check if active pokemon has fainted and update reward
+#   switch pokemon
 
 
--- pokemon_data.py
+++ pokemon_data.py
@@ -2,20 +2,29 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-def getActivePokemon(browser,actions):
-    active_pokemon = browser.find_element_by_name('chooseDisabled')
-
+def getActivePokemon(browser):
+    active_pokemon = WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.NAME, "chooseDisabled")))#browser.find_element_by_name('chooseDisabled')
+    actions = ActionChains(browser)
+    print('\nActive Pokemon text:' + active_pokemon.text)
 
     if 'active' in active_pokemon.get_attribute('value').split(','):
         pokemon = {}
 
-        time.sleep(3)
+        # time.sleep(3)
+        try:
+            actions.move_to_element(active_pokemon).perform()
+        except:
+            active_pokemon = WebDriverWait(browser, 60).until(EC.presence_of_element_located((By.NAME, "chooseDisabled")))#browser.find_element_by_name('chooseDisabled')
+            actions.move_to_element(active_pokemon).perform()
 
-        actions.move_to_element(active_pokemon).perform()
+        pokemon['element'] = active_pokemon
 
-        time.sleep(3)
+        # time.sleep(3)
 
         stats = browser.find_element_by_class_name('tooltip')
         name_level = stats.find_element_by_xpath('.//h2')
@@ -94,16 +103,21 @@ def getActivePokemon(browser,actions):
                 print('Moves:' + str(moves))
         pokemon['isActive'] = True
         pokemon['isAlive'] = True
+    
+    else:
+        return {'isAlive':False}
+    
     return pokemon
 
-def getInactivePokemon(browser,actions):
+def getInactivePokemon(browser):
     my_pokemon = []
-    switch_pokemon_list = browser.find_elements_by_name('chooseSwitch')
-
+    actions = ActionChains(browser)
+    switch_pokemon_list = WebDriverWait(browser, 60).until(EC.presence_of_all_elements_located((By.NAME, "chooseSwitch")))#browser.find_elements_by_name('chooseSwitch')
     for i in range(len(switch_pokemon_list)):
         pokemon = {}
 
         actions.move_to_element(switch_pokemon_list[i]).perform()
+        pokemon['element'] = switch_pokemon_list[i]
 
         stats = browser.find_element_by_class_name('tooltip')
         name_level = stats.find_element_by_xpath('.//h2')
@@ -185,14 +199,18 @@ def getInactivePokemon(browser,actions):
                 print('Moves:' + str(moves))
             elif i==4: #modified stats
                 print('Stats Modified:' + stats[i].text)
-        pokemon['isActive'] = True
+        pokemon['isActive'] = False
         pokemon['isAlive'] = True
         my_pokemon.append(pokemon)
     return my_pokemon
 
-def getActiveEnemyPokemon(browser,actions):
+def getActiveEnemyPokemon(browser):
     pokemon = {}
-    divs = browser.find_element_by_class_name('tooltips')
+    actions = ActionChains(browser)
+    divs = None
+    divs = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "tooltips")))#browser.find_element_by_class_name('tooltips')
+    if divs == None:
+        return None
     divs = divs.find_elements_by_class_name('has-tooltip')
     for x in divs:
         if x.get_attribute('data-tooltip') == 'activepokemon|1|0':
@@ -230,37 +248,163 @@ def getActiveEnemyPokemon(browser,actions):
             hp = float(stats[i].text.split('%')[0][4:])
             pokemon['hp'] = hp
             print('Hp:' + str(hp))
-        elif i==2: #stats spe 162 to 205
-            s = stats[i].text.split('to')
-            from_spe = int(s[0][4:-1])
-            to_spe = s[1][1:]
-            to_spe = int(to_spe.split(' ')[0])
+        elif i==2: #stats spe 162 to 205 or item
+                if stats[i].text[0:4] == 'Item':
+                    pass
+                else:
+                    s = stats[i].text.split('to')
+                    from_spe = int(s[0][4:-1])
+                    to_spe = s[1][1:]
+                    to_spe = int(to_spe.split(' ')[0])
 
-            pokemon['from_spe'] = from_spe
-            pokemon['to_spe'] = to_spe
+                    pokemon['from_spe'] = from_spe
+                    pokemon['to_spe'] = to_spe
 
-            print('Stats: spe ' + str(pokemon['from_spe']) + ' to ' + str(pokemon['to_spe']))
+                    print('Stats: spe ' + str(pokemon['from_spe']) + ' to ' + str(pokemon['to_spe']))
 
+        elif i==3: #stats spe 162 to 205
+            if stats[i].text[0:3] != 'spe':
+                pass
+            else:
+                s = stats[i].text.split('to')
+                from_spe = int(s[0][4:-1])
+                to_spe = s[1][1:]
+                to_spe = int(to_spe.split(' ')[0])
+
+                pokemon['from_spe'] = from_spe
+                pokemon['to_spe'] = to_spe
+
+                print('Stats: spe ' + str(pokemon['from_spe']) + ' to ' + str(pokemon['to_spe']))
+    
     pokemon['Atk'] = 1.0
     pokemon['Def'] = 1.0
     pokemon['SpA'] = 1.0
     pokemon['SpD'] = 1.0
     pokemon['Spe'] = 1.0
 
+    try:
+        status = WebDriverWait(browser, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "status")))
+        bads = WebDriverWait(status, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "bad")))
+        for bad in bads:
+            bad_list = bad.text.split('x')
+            bad_amount = float(bad_list[0])
+            bad_stat = bad_list[1].split(';')[1]
+            pokemon[bad_stat] = bad_amount
 
-    status = browser.find_element_by_class_name('status')
-    bads = browser.find_elements_by_class_name('bad')
-    for bad in bads:
-        bad_list = bad.text.split(x)
-        bad_amount = float(bad_list[0])
-        bad_stat = bad_list[1].split(';')[1]
-        pokemon[bad_stat] = bad_amount
+        goods = WebDriverWait(status, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "good")))
+        for good in goods:
+            good_list = good.text.split('x')
+            good_amount = float(good_list[0])
+            good_stat = good_list[1].split(';')[1]
+            pokemon[good_stat] = good_amount
 
-    goods = browser.find_elements_by_class_name('good')
-    for good in goods:
-        good_list = good.text.split(x)
-        good_amount = float(good_list[0])
-        good_stat = good_list[1].split(';')[1]
-        pokemon[good_stat] = good_amount
+    except Exception as e:
+        print('Exception, no status found for active enemy pokemon\n')
+    
 
     return pokemon
+
+def getEnemyPokemonList(browser):
+    enemy_pokemon = []
+    actions = ActionChains(browser)
+    rightbar = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "rightbar")))#browser.find_element_by_class_name('rightbar')
+    team = rightbar.find_elements_by_class_name('picon has-tooltip')
+
+    for p in team:
+        pokemon = {}
+        hasFainted = False
+        faintedCount = 0
+        actions.move_to_element(p).perform()
+        stats = browser.find_element_by_class_name('tooltip')
+        name_level = stats.find_element_by_xpath('.//h2')
+        type_list = name_level.find_elements_by_xpath('.//img')
+        name_level = name_level.text.split(' ')
+
+        pokemon['element'] = p
+
+        name = ''
+        for i in range(len(name_level) - 1):
+            name+= name_level[i] + ' '
+        name = name[:len(name)-1] #Name of pokemon
+        pokemon['name'] = name
+        print('Name:' + name)
+
+        level = int(name_level[-1][1:]) #level
+        pokemon['level'] = level
+        print('Level:' + str(level))
+        type = []
+        for t in type_list:
+            if t.get_attribute('alt') != 'M' and t.get_attribute('alt') != 'F':
+                type.append(t.get_attribute('alt'))
+
+        pokemon['type'] = type
+        print('Type:' + str(type))
+
+        stats = stats.find_elements_by_xpath('.//p')
+
+        for i in range(len(stats)):
+            if i==0: #hp
+                if stats[i].text.contains('fainted'):
+                    hasFainted = True
+                    faintedCount+=1
+                    break
+                hp = float(stats[i].text.split('%')[0][4:])
+                pokemon['hp'] = hp
+                print('Hp:' + str(hp))
+            elif i==2: #stats spe 162 to 205 or item
+                if stats[i].text[0:4] == 'Item':
+                    pass
+                else:
+                    s = stats[i].text.split('to')
+                    from_spe = int(s[0][4:-1])
+                    to_spe = s[1][1:]
+                    to_spe = int(to_spe.split(' ')[0])
+
+                    pokemon['from_spe'] = from_spe
+                    pokemon['to_spe'] = to_spe
+
+                    print('Stats: spe ' + str(pokemon['from_spe']) + ' to ' + str(pokemon['to_spe']))
+
+            elif i==3: #stats spe 162 to 205
+                if stats[i].text[0:3] != 'spe':
+                    pass
+                else:
+                    s = stats[i].text.split('to')
+                    from_spe = int(s[0][4:-1])
+                    to_spe = s[1][1:]
+                    to_spe = int(to_spe.split(' ')[0])
+
+                    pokemon['from_spe'] = from_spe
+                    pokemon['to_spe'] = to_spe
+
+                    print('Stats: spe ' + str(pokemon['from_spe']) + ' to ' + str(pokemon['to_spe']))
+
+        if hasFainted == False:
+            pokemon['Atk'] = 1.0
+            pokemon['Def'] = 1.0
+            pokemon['SpA'] = 1.0
+            pokemon['SpD'] = 1.0
+            pokemon['Spe'] = 1.0
+
+            try:
+                status = WebDriverWait(browser, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "status")))#browser.find_element_by_class_name('status')
+                bads = WebDriverWait(status, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "bad")))#browser.find_elements_by_class_name('bad')
+                for bad in bads:
+                    bad_list = bad.text.split(x)
+                    bad_amount = float(bad_list[0])
+                    bad_stat = bad_list[1].split(';')[1]
+                    pokemon[bad_stat] = bad_amount
+
+                goods = WebDriverWait(status, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "good")))#browser.find_elements_by_class_name('good')
+                for good in goods:
+                    good_list = good.text.split(x)
+                    good_amount = float(good_list[0])
+                    good_stat = good_list[1].split(';')[1]
+                    pokemon[good_stat] = good_amount
+                enemy_pokemon.append(pokemon)
+            except Exception as e:
+                print('Exception no status found for enemy inactive pokemon\n')
+    return enemy_pokemon,faintedCount
+        
+
+
