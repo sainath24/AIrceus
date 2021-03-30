c2e82e2f4da80388a6be68f562f346bde0cd781c
Sainath Prasanna <sainathprasanna@gmail.com>
2021-03-31 01:46:37 +0530

    simplify get_move_data

--- get_move_data.js
+++ get_move_data.js
@@ -1,14 +1,2 @@
 import {Moves} from "./moves.js"
-import * as readline from "readline" 
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-   
-  rl.question('', id => {
-    console.log(JSON.stringify(Moves[id]));
-    rl.close();
-  });
-
-// console.log(Moves[process.argv[2]])
+console.log(JSON.stringify(Moves[process.argv[2]]))
