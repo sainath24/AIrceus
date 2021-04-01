17bfca9b8eec253ec27f286c2b8731b8d9197060
Sainath Prasanna <sainathprasanna@gmail.com>
2021-04-01 23:37:22 +0530

    modify main to run multiple episodes

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from Translator import Translator
 from Brain import Brain
 
 from multiprocessing import Queue
+from config import config
 
-data_queue = Queue()
-action_queue = Queue()
+data_queue = None
+action_queue = None
 
 batsim = BattleSimulator(data_queue=data_queue, action_queue= action_queue)
 brain = Brain(train=True)
@@ -15,6 +16,16 @@ translator = Translator(action_queue=action_queue)
 game = Game()
 ingestor = Ingestor(data_queue=data_queue, translator=translator, brain=brain, game=game)
 
-batsim.start(threaded=True)
-ingestor.start(threaded=True)
+for episode in range(config['episodes']):
+    print('\nEPISODE: ', episode, '\n')
+    data_queue = Queue()
+    action_queue = Queue()
+    batsim.set_queues(data_queue, action_queue)
+    ingestor.set_queue(data_queue)
+    translator.set_queue(action_queue)
+
+    game.reset()
+
+    batsim.start(threaded=True)
+    ingestor.start(threaded=True) # WILL NOT PASS HERE UNTIL INGESTOR ENDS
 
