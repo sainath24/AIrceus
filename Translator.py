b3769a160310f93b2dd2149d7ba37b28199a9dbd
Sainath Prasanna <sainathprasanna@gmail.com>
2021-04-03 20:31:52 +0530

    translate move action to move name

--- Translator.py
+++ Translator.py
@@ -1,3 +1,4 @@
+import logging
 class Translator:
     def __init__(self, action_queue) -> None:
         self.action_q = action_queue
@@ -9,10 +10,21 @@ class Translator:
 
         return name
 
+    def get_move_name(self, pokemon_list, position):
+        move_name = None
+        for pokemon in pokemon_list:
+            if pokemon.active and pokemon.hp != 0.0: # ACTIVE POKEMON
+                move_name = pokemon.moves[position].name
+        
+        if move_name == None:
+            logging.error('MOVE NAME NOT FOUND AT POSITION ' + str(position) + 'FOR POKEMON: ' + str(pokemon.get_dict()))
+        return move_name
+
+
     def translate(self, player_identifier, action, pokemon_list):
         output = '>' + player_identifier
         if action < 4: # MOVE
-            output = output + ' move ' + str(int(action) + 1) + '\n'
+            output = output + ' move ' + str(self.get_move_name(pokemon_list, int(action))) + '\n'
         else: # SWITCH
             output = output + ' switch ' + str(self.get_switch_name(pokemon_list, int(action) - 4)) + '\n'
 
