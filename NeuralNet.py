da5feb8d6d0832736360182e6940ffd1f4bec629
Sainath Prasanna <sainathprasanna@gmail.com>
2021-06-11 02:51:40 +0530

    shrink network, split actor and critic
    separate critic state to predict state value, lstm only for actor

--- NeuralNet.py
+++ NeuralNet.py
@@ -3,27 +3,28 @@ from torch._C import device
 import torch.nn as nn
 
 class NeuralNet(nn.Module):
-    def __init__(self, state_size, action_size, hidden_size, lstm_size, device):
+    def __init__(self, state_size, critic_state_size, action_size, hidden_size, lstm_size, device):
         super(NeuralNet, self).__init__()
         self.state_size = state_size
+        self.critic_state_size = critic_state_size
         self.hidden_size = hidden_size
         self.action_size = action_size
         self.lstm_size = lstm_size
         self.device = device
 
-        self.lstm = nn.LSTM(state_size, hidden_size, lstm_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, lstm_size)
 
         self.actor = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
-            nn.Linear(hidden_size, action_size)
+            nn.Linear(state_size, hidden_size), nn.ReLU()
+            # nn.Linear(hidden_size, hidden_size), nn.ReLU(),
+            # nn.Linear(hidden_size, hidden_size), nn.ReLU(),
+            # nn.Linear(hidden_size, action_size)
         )
 
         self.critic = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
+            nn.Linear(self.critic_state_size, hidden_size), nn.ReLU(),
+            # nn.Linear(hidden_size, hidden_size), nn.ReLU(),
+            # nn.Linear(hidden_size, hidden_size), nn.ReLU(),
             nn.Linear(hidden_size, 1)
         )
 
@@ -33,13 +34,15 @@ class NeuralNet(nn.Module):
         self.lstm_hidden = (torch.zeros(self.lstm_size,1,self.hidden_size, device=self.device), torch.zeros(self.lstm_size,1,self.hidden_size, device=self.device))
 
 
-    def forward(self, nn_input, lstm_hidden = None):
+    def forward(self, nn_input, critic_input, lstm_hidden = None):
         # print('\nNN INPUT SIZE BEFORE CONVERSION: ', nn_input.size())
         # nn_input = nn_input.view(1,1,self.state_size)
         nn_input = nn_input.unsqueeze(0)
+
+        out = self.actor(nn_input)
         # print('\nSIZE OF INPUT: ', nn_input.size())
         if lstm_hidden == None:
-            output, self.lstm_hidden = self.lstm(nn_input, self.lstm_hidden)
+            output, self.lstm_hidden = self.lstm(out, self.lstm_hidden)
             # print('\nLSTM HIDDEN LEN: ',len(self.lstm_hidden))
             # print('\nLSTM HIDDEN SIZE: ', self.lstm_hidden[0].size())
         else:
@@ -49,14 +52,11 @@ class NeuralNet(nn.Module):
             # print('\nLEN OF UPDATE LSTM AFTER UNBIND: ', len(lstm_hidden))
             # print('\nUPDATE LSTM AFTER UNBIND: ', lstm_hidden[0].size(), lstm_hidden[1].size())
             # lstm_hidden = [x.unbind() for x in lstm_hidden]
-            output, hidden = self.lstm(nn_input, lstm_hidden)
+            output, hidden = self.lstm(out, lstm_hidden)
         
         # print('\nLSTM OUTPUT: ', output.size())
-        output = output.squeeze()
+        actor_output = output.squeeze()
         # print('\nNN INPUT: ', output.size())
-
-
-        actor_output = self.actor(output)
-        critic_output = self.critic(output)
+        critic_output = self.critic(critic_input)
 
         return actor_output, critic_output, self.lstm_hidden
