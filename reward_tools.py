321c326bda5aa4bb71599242b26e7333e1ff21b6
Sainath Prasanna <sainathprasanna@gmail.com>
2021-06-21 20:18:43 +0530

    implement new rewards
    0.5 when enemy pokemon faints,0.5 if tie,1 for win,0 for loss

--- reward_tools.py
+++ reward_tools.py
@@ -1,7 +1,7 @@
 import torch
 # import logging
 
-def get_reward(new_state, game):
+def get_reward(new_state, game, enemy_alive):
     # NEW STATE HAS SIZE 840, FIRST 420 is agent pokemon, next 420 is enemy pokemon
     # SINCE STATE HAS VALUES OF RANGE [0,1], max is 420, min is 0
     # MAX REWARD IS 420, MIN REWARD IS -420, normalize using these values
@@ -17,35 +17,40 @@ def get_reward(new_state, game):
     agent_state, enemy_state = torch.split(new_state, int(new_state.size()[0]/2))
     # logging.debug('AGENT STATE SIZE: '+ str(agent_state.size()))
     # logging.debug('ENEMY STATE SIZE: ' +  str(enemy_state.size()))
+    new_enemy_alive = [1 if enemy_state[i]>0 else 0 for i in range(0,enemy_state.size(0),61)]
+    # print(f'New enemy alive: {new_enemy_alive}')
 
-    agent_sum = agent_state.sum()
-    enemy_sum = enemy_state.sum()
+    reward = sum([a-b for a,b in zip(enemy_alive, new_enemy_alive)]) * 0.5
 
-    reward = ((agent_weight*agent_sum) - (enemy_weight*enemy_sum)).item()
+    # agent_sum = agent_state.sum()
+    # enemy_sum = enemy_state.sum()
+
+    # reward = ((agent_weight*agent_sum) - (enemy_weight*enemy_sum)).item()
     if game.tie: # ADD BONUS
         # reward += tie_bonus
         reward = 0.5
-        reward = torch.clamp(torch.tensor(reward), 0.0, 1.0)
-        return reward
+        # reward = torch.clamp(torch.tensor(reward), 0.0, 1.0)
+        # return reward
         # logging.debug('RECEIVE TIE BONUS')
     elif game.win == 'p1': # AGENT WON
         # logging.debug('RECEIVE WIN BONUS')
         # reward += win_bonus
         reward = 1
-        reward = torch.clamp(torch.tensor(reward), 0.0, 1.0)
-        return reward
+        # reward = torch.clamp(torch.tensor(reward), 0.0, 1.0)
+        # return reward
     elif game.win == 'p2': # AGENT LOST
         # logging.debug('RECEIVE LOSE BONUS')
         # reward += lose_bonus
         reward = 0
-        reward = torch.clamp(torch.tensor(reward), 0.0, 1.0)
-        return reward
+        # reward = torch.clamp(torch.tensor(reward), 0.0, 1.0)
+        # return reward
     
-    minimum = -int(new_state.size()[0]/2)
-    maximum = int(new_state.size()[0]/2)
+    # minimum = -int(new_state.size()[0]/2)
+    # maximum = int(new_state.size()[0]/2)
     
-    reward = torch.clamp(torch.tensor(reward), minimum, maximum)
-    reward = (reward - minimum)/(maximum - minimum)
+    # reward = torch.clamp(torch.tensor(reward), minimum, maximum)
+    # reward = (reward - minimum)/(maximum - minimum)
 
     # logging.debug('REWARD:' + str(reward))
-    return reward
+    # print(f'reward: {reward}')
+    return torch.tensor(reward, dtype = torch.float), new_enemy_alive
